82a742acf3370abf3600e42891b65cf676cddf27
mac <18644056528@163.com>
2018-08-13 12:52:32 +0800

    ----------插件化 基类库---------- 1、修改gradle版本 2、rxJava rxAndroid rxPermissions rxLifecycle 3、baseActivity baseFragment basePresenter IPresenter  IView 4、SoLoaderUtil 5、Retrofit 网络请求框架封装 ---Log显示 ---GET/POST请求 ---文件下载回调监听 ---修复无法接受到下载回调 ---修复Retrofit无法显示打印日志问题 ---修复下载无法关闭导致内存泄露问题 ---认证后的RetrofitClient ---修改请求失败回调逻辑 6、RxSchedulers 线程调度 7、RxBus(基于Blankj 的RxBus 修改) ---使用范例 8、颜色值 9、Glide 封装 加载进度回调 10、theme style 文件 11、状态栏工具类 12、安装插件工具类（InstallPlugComponentUtil.kt） 13、SmartRefreshLayout 14、Lottie 15、CircleImageView 16、BaseRecyclerViewAdapter

--- base_module/src/main/java/com/junlong0716/base/module/http/base/RequestCallback.kt
+++ base_module/src/main/java/com/junlong0716/base/module/http/base/RequestCallback.kt
@@ -25,7 +25,7 @@ interface RequestCallback<T> {
      * 请求数据失败，指在请求网络API接口请求方式时，出现无法联网、
      * 缺少权限，内存泄露等原因导致无法连接到请求数据源。
      */
-    fun onError()
+    fun onError(msg:String)
 
     /**
      * 当请求数据结束时，无论请求结果是成功，失败或是抛出异常都会执行此方法给用户做处理，通常做网络
--- base_module/src/main/java/com/junlong0716/base/module/http/base/RequestJsonCallback.kt
+++ base_module/src/main/java/com/junlong0716/base/module/http/base/RequestJsonCallback.kt
@@ -25,5 +25,5 @@ interface RequestJsonCallback {
      * 请求数据失败，指在请求网络API接口请求方式时，出现无法联网、
      * 缺少权限，内存泄露等原因导致无法连接到请求数据源。
      */
-    fun onError()
+    fun onError(msg:String)
 }
