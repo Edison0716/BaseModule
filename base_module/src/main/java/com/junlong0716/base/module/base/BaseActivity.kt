2645a793cb8b73d53cb724e4a4592d2f435b9828
mac <18644056528@163.com>
2018-08-31 13:57:37 +0800

    ----------插件化 基类库----------
    1、修改gradle版本
    2、rxJava rxAndroid rxPermissions rxLifecycle
    3、baseActivity baseFragment basePresenter IPresenter  IView
    4、SoLoaderUtil
    5、Retrofit 网络请求框架封装
    ---Log显示
    ---GET/POST请求
    ---文件下载回调监听
    ---修复无法接受到下载回调
    ---修复Retrofit无法显示打印日志问题
    ---修复下载无法关闭导致内存泄露问题
    ---认证后的RetrofitClient
    ---修改请求失败回调逻辑
    ---插件开发模式 网络请求切换
    ---账号过期 RxBus 推送
    6、RxSchedulers 线程调度
    7、RxBus(基于Blankj 的RxBus 修改)
    ---使用范例
    8、颜色值
    9、Glide 封装 加载进度回调
    10、theme style 文件
    11、状态栏工具类
    12、安装插件工具类（InstallPlugComponentUtil.kt）
    13、SmartRefreshLayout
    14、Lottie
    15、CircleImageView
    16、BaseRecyclerViewAdapter
    17、cardview
    18、圆角Banner、DeleteEditText
    19、BannerImageLoaderManager
    20、加载弹窗 加载动画 .json 文件
    ---沉浸式 dialog
    21、共享属性Manager
    22、空布局
    23、弹窗动画
    24、行间距 ItemDecoration DividerItemDividerDecoration
    25、字体对齐的textview
    26、ReboundScrollView
    27、BitmapConvertUtil
    28、分享图片 .svg文件
    29、集成Unity ads
    30、移除弹窗毛玻璃背景（UI不喜欢）
    31、update utilcode to 1.19.4
    32、适配屏幕

--- base_module/src/main/java/com/junlong0716/base/module/base/BaseActivity.kt
+++ base_module/src/main/java/com/junlong0716/base/module/base/BaseActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v7.app.AppCompatActivity
+import com.blankj.utilcode.util.ScreenUtils
 import com.junlong0716.base.module.manager.ActivityManager
 import com.junlong0716.base.module.rx.bus.RxBus
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
@@ -34,8 +35,19 @@ abstract class BaseActivity<P : IPresenter> : RxAppCompatActivity() {
         //注册RxBus
         registerRxBus()
 
+        //统一适配屏幕
+        adaptScreen()
+
         //初始化控件
         initView(savedInstanceState)
+    }
+
+    private fun adaptScreen() {
+        if (ScreenUtils.isPortrait()) {
+            ScreenUtils.adaptScreen4VerticalSlide(this, 408)
+        } else {
+            ScreenUtils.adaptScreen4HorizontalSlide(this, 408)
+        }
     }
 
     abstract fun beforeSetLayout()
@@ -54,8 +66,11 @@ abstract class BaseActivity<P : IPresenter> : RxAppCompatActivity() {
         //解除Presenter 与 Activity 绑定
         this.mPresenter!!.onDestroy()
         this.mPresenter = null
+
         //解除注册RxBus
         RxBus.default.unregister(this)
+
+        ScreenUtils.cancelAdaptScreen(this)
     }
 
 
